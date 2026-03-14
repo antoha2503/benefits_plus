a20ab5169caf0c050d179a6394ab44c39073f4a3
antoha2503 <114880081+antoha2503@users.noreply.github.com>
2026-03-14 14:30:20 +0200

    Delete benefits_plus directory

--- benefits_plus/model/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import benefits_dashboard
-from . import category_compensation
-from . import create_user_compensation_wizard
-from . import user_compensation
-from . import custom_field_HrEmployee
-from . import edit_total_compensation
--- benefits_plus/model/category_compensation.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-class CategoryCompensation(models.Model):
-    _name = "category.compensation"
-    _description = "Category Compensation"
-    _rec_name = "name"
-
-    name = fields.Char(string="Name", required=True)
