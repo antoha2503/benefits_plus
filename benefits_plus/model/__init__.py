ceb9c92fb244cd502d01022182b39be38cab2848
antoha2503 <114880081+antoha2503@users.noreply.github.com>
2026-03-14 02:57:47 +0200

    Add files via upload

---
+++ benefits_plus/model/__init__.py
@@ -0,0 +1,7 @@
+from . import benefits_dashboard
+from . import category_compensation
+from . import create_user_compensation_wizard
+from . import user_compensation
+from . import custom_field_HrEmployee
+from . import edit_total_compensation
+from . import res_user
